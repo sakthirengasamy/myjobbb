7008be63adb3ae2199ddfb1d4c7f4b420b49a9e2
Prasanthkarthick <prasanthkarthik20@gmail.com>
2015-02-16 12:27:41 +0530

    Android simple project committed

---
+++ gen/R.java.d
@@ -0,0 +1,10 @@
+D:\jenkins\Github\trunk\gen\com\example\androidtestapp\R.java \
+ : D:\jenkins\Github\trunk\res\drawable-hdpi\ic_launcher.png \
+D:\jenkins\Github\trunk\res\drawable-mdpi\ic_launcher.png \
+D:\jenkins\Github\trunk\res\drawable-xhdpi\ic_launcher.png \
+D:\jenkins\Github\trunk\res\drawable-xxhdpi\ic_launcher.png \
+D:\jenkins\Github\trunk\res\layout\activity_main.xml \
+D:\jenkins\Github\trunk\res\values\dimens.xml \
+D:\jenkins\Github\trunk\res\values\strings.xml \
+D:\jenkins\Github\trunk\res\values-w820dp\dimens.xml \
+D:\jenkins\Github\trunk\bin\AndroidManifest.xml \
